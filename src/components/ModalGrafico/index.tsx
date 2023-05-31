45e9287155b0fd9f835822bc892aa781023250e6
Leonardo <leolopesribeiro05@gmail.com>
2023-05-31 11:42:44 -0300

    Modal do gráfico

--- src/components/ModalGrafico/index.tsx
+++ src/components/ModalGrafico/index.tsx
@@ -1,12 +1,18 @@
-import { Text, View, TouchableOpacity, ScrollView, Dimensions } from "react-native";
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  ScrollView,
+  Dimensions,
+} from "react-native";
 import { useEffect, useState } from "react";
+import { AntDesign } from "@expo/vector-icons";
 import Modal from "react-native-modal";
 import { LineChart } from "react-native-chart-kit";
-import { VictoryChart, VictoryLine, VictoryTheme } from "victory-native";
 import { api } from "../../services/api";
 import moment from "moment";
 import { LineChartData } from "react-native-chart-kit/dist/line-chart/LineChart";
-import { CurrentCoins } from "../../types/current";
+import { CurrentCoins, DataType } from "../../types/current";
 interface PropsType {
   visibilidade: boolean;
   data: CurrentCoins;
@@ -18,33 +24,34 @@ export default function ModalGrafico({
   toggle,
   data,
 }: PropsType) {
-  const [dataGrafico, setDataGrafico] = useState<LineChartData | null
-  >(null);
+  const [dataGrafico, setDataGrafico] = useState<LineChartData | null>(null);
   const dataAnterior = moment().subtract(7, "days").calendar();
   const datahj = moment().format("YYYYMMDD");
 
   const dataAnteriorFormatada = dataAnterior.split("/");
-  console.log(
-    dataAnteriorFormatada[2],
-    dataAnteriorFormatada[0],
-    dataAnteriorFormatada[1]
-  );
+
   useEffect(() => {
     async function getData() {
       try {
         const response = await api.get(
-          `/${data.code}-${data.codein}/7?start_date=${dataAnteriorFormatada[2]}${dataAnteriorFormatada[0]}${dataAnteriorFormatada[1]}&end_date=${datahj}`
+          `/${data.code}-${data.codein}/5?start_date=${dataAnteriorFormatada[2]}${dataAnteriorFormatada[0]}${dataAnteriorFormatada[1]}&end_date=${datahj}`
         );
-        console.log( `/${data.code}-${data.codein}/7?start_date=${dataAnteriorFormatada[2]}${dataAnteriorFormatada[0]}${dataAnteriorFormatada[1]}&end_date=${datahj}`);
-        const dataGraficoLocal: any = { labels: [], datasets: [{ data: [],  }] };
-        response.data.forEach((item: any, index: any) => {
+        // console.log(response.data)
+        // console.log( `/${data.code}-${data.codein}/5?start_date=${dataAnteriorFormatada[2]}${dataAnteriorFormatada[0]}${dataAnteriorFormatada[1]}&end_date=${datahj}`);
+        const dataGraficoLocal: DataType = {
+          labels: [],
+          datasets: [{ data: [] }],
+        };
+        response.data.forEach((item: CurrentCoins, index: number) => {
           dataGraficoLocal.labels.push(
             moment(response.data[0].create_date)
               .subtract(index, "days")
               .format("DD/MM")
           );
-          dataGraficoLocal.datasets[0].data.push(Number(item.bid))
+          dataGraficoLocal.datasets[0].data.push(Number(item.bid));
         });
+        dataGraficoLocal.labels.reverse();
+        console.log(dataGraficoLocal.datasets[0].data);
         setDataGrafico(dataGraficoLocal);
       } catch (error) {
         console.log(error);
@@ -53,51 +60,71 @@ export default function ModalGrafico({
     getData();
   }, []);
   return (
-    <ScrollView> 
-      <Modal isVisible={visibilidade} className="m-2 w-full flex justify-evenly">
-        <View className="w-full m-0">
-     
-        {dataGrafico != null && (
+    <ScrollView>
+      <Modal isVisible={visibilidade} className="m-2 w-full flex">
+        <View className="flex bg-cores right-2 m-2 rounded-md">
+          <View className="flex flex-row justify-between">
+            <TouchableOpacity
+              onPress={toggle}
+              className="flex w-min items-start top-3 left-3"
+            >
+              <AntDesign
+                className=" text"
+                name="closecircleo"
+                size={24}
+                color="#C9C9C9"
+              />
+            </TouchableOpacity>
+            <TouchableOpacity className="w-min flex items-end right-5 top-3">
+              <AntDesign name="hearto" size={24} color="#C9C9C9" />
+            </TouchableOpacity>
+          </View>
+          <Text className="text-gray font-bold border-b border-golden top-5 text-center my-3">
+            {data.name}
+          </Text>
+          <View className="w-full m-0">
+            {dataGrafico != null ? (
               <LineChart
-              data={dataGrafico}
-              width={Dimensions.get('window').width - 15} // from react-native
-              height={300}
-              yAxisLabel={'R$'} 
-              yLabelsOffset={5}
-              
-              chartConfig={{
-                // backgroundColor: '#1cc910',
-                backgroundGradientFrom: '#1C1C1C',
-                backgroundGradientTo: '#1C1C1C',
-               fillShadowGradient: "#C9C9C9",
-               fillShadowGradientFromOffset: 1,
-                decimalPlaces: 3, // optional, defaults to 2dp
-                color: (opacity = 1) => `rgba(184, 157, 89, ${opacity})`,
-                labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-                style: {
+                data={dataGrafico}
+                width={Dimensions.get("window").width - 15} // from react-native
+                height={300}
+                yAxisLabel={"R$"}
+                // yLabelsOffset={-10}
+                chartConfig={{
+                  // backgroundColor: '#1cc910',
+                  backgroundGradientFrom: "#1C1C1C",
+                  backgroundGradientTo: "#1C1C1C",
+                  fillShadowGradient: "#C9C9C9",
+                  propsForHorizontalLabels: {
+                    x: 60,
+                  },
+                  fillShadowGradientFromOffset: 1,
+                  decimalPlaces: 3, // optional, defaults to 2
+                  color: (opacity = 1) => `rgba(184, 157, 89, ${opacity})`,
+                  labelColor: (opacity = 1) =>
+                    `rgba(255, 255, 255, ${opacity})`,
+                  style: {
+                    borderRadius: 16,
+                  },
+                  propsForLabels: {
+                    fontSize: 10,
+                  },
+                  // propsForDots:{
+                  //   r: "4",
+                  //   strokeWidth: "2",
+                  //   stroke: "#B89D59"
+                  // }
+                }}
+                bezier
+                style={{
+                  paddingTop: 30,
+                  marginVertical: 8,
                   borderRadius: 16,
-                },
-                // propsForDots:{
-                //   r: "4",
-                //   strokeWidth: "2",
-                //   stroke: "#B89D59"
-                // }
-              }}
-              bezier
-              style={{
-                paddingTop: 30,
-                marginVertical: 8,
-                borderRadius: 16,
-              }}
-            />
-             
-            )}
-         
+                }}
+              />
+            ) : null}
+          </View>
         </View>
-        <TouchableOpacity onPress={toggle} className=" w-full flex items-center border rounded-md r">
-            <Text className=" text-golden">X</Text>
-           
-          </TouchableOpacity>
       </Modal>
     </ScrollView>
   );
